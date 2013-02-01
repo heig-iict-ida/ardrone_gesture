190351082f99bfabbec1076313f43855bac4311e
Julien Rebetez <julien.rebetez@heig-vd.ch>
2013-02-01 10:55:34 +0100

    Fix DTW. Add a 3-axis test based on data from dataset and results from python's mlpy implementation

--- src/madsdf/ardrone/gesture/DTW.java
+++ src/madsdf/ardrone/gesture/DTW.java
@@ -3,6 +3,8 @@
  * and open the template in the editor.
  */
 package madsdf.ardrone.gesture;
+import static com.google.common.base.Preconditions.*;
+import java.util.Arrays;
 
 /**
  * Dynamic time warping implementation
@@ -12,18 +14,24 @@ public class DTW {
         return Math.min(Math.min(a, b), c);
     }
     
+    // DTW Distance between 2 series
     public static float dtwDistance(float[] serie1, float[] serie2) {
         final int N = serie1.length;
         final int M = serie2.length;
         final float D[][] = new float[N][M];
-        for (int i = 0 ; i < N; ++i) {
-            D[i][0] = Float.MAX_VALUE;
+        // Instead of initializing the first line and row to infinity and having
+        // a (N+1) * (M+1) matrix, we "merge" the two first lines and therefore
+        // directly initialize to the distance.
+        // That's inspired by mlpy's implementation (and a bit different from
+        // Wikipedia's implementation which uses 1-based indexing for serie and
+        // 0-based indexing for cost matrix
+        D[0][0] = Math.abs(serie1[0] - serie2[0]);
+        for (int i = 1 ; i < N; ++i) {
+            D[i][0] = D[i-1][0] + Math.abs(serie1[i] - serie2[0]);
         }
-        for (int i = 0; i < M; ++i) {
-            D[0][i] = Float.MAX_VALUE;
-            
+        for (int i = 1; i < M; ++i) {
+            D[0][i] = D[0][i-1] + Math.abs(serie1[0] - serie2[i]);
         }
-        D[0][0] = 0;
         
         for (int i = 1; i < N; ++i) {
             for (int j = 1; j < M; ++j) {
@@ -33,6 +41,18 @@ public class DTW {
                                       D[i-1][j-1]); // match
             }
         }
+        //System.out.println(Arrays.deepToString(D));
         return D[N-1][M-1];
+    }
+    
+    // sum of DTW distance between corresponding axis of two series
+    // data is NxM where N is the number of axis and M the number of values
+    public static float allAxisDTW(float[][] serie1, float[][] serie2) {
+        checkState(serie1.length == serie2.length);
+        float sum = 0;
+        for (int i = 0; i < serie1.length; ++i) {
+            sum += dtwDistance(serie1[i], serie2[i]);
+        }
+        return sum;
     }
 }
